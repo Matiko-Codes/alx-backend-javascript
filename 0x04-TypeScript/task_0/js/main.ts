d09306796c0dbb0c114ce891a5b3b43af44b6049
Matiko-Codes <mmatiko243@gmail.com>
2023-02-01 18:23:47 +0300

    Documented version

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,10 +1,12 @@
+// Define the Student interface with four properties: firstName, lastName, age, and location
 interface Student {
-    firstName: string;
-    lastName: string;
-    age: number;
-    location: string;
+    firstName: string;   // first name of the student, type string
+    lastName: string;    // last name of the student, type string
+    age: number;         // age of the student, type number
+    location: string;    // location of the student, type string
   }
   
+  // Create two student objects that conform to the Student interface
   const student1: Student = {
     firstName: "John",
     lastName: "Doe",
@@ -19,22 +21,33 @@ interface Student {
     location: "London"
   };
   
+  // Store the two student objects in an array named studentsList
   const studentsList: Student[] = [student1, student2];
   
+  // Create a table element
   const table = document.createElement("table");
   
+  // Iterate through each student in the studentsList array
   studentsList.forEach(student => {
+    // Create a row element
     const row = document.createElement("tr");
+  
+    // Create two cells, one for the first name and one for the location
     const firstNameCell = document.createElement("td");
     const locationCell = document.createElement("td");
   
+    // Set the innerHTML of the cells to the first name and location of the current student
     firstNameCell.innerHTML = student.firstName;
     locationCell.innerHTML = student.location;
   
+    // Append the cells to the row
     row.appendChild(firstNameCell);
     row.appendChild(locationCell);
   
+    // Append the row to the table
     table.appendChild(row);
   });
   
-  document.body.appendChild(table);  
+  // Append the table to the body of the HTML document
+  document.body.appendChild(table);
+  
