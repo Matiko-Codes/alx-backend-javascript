0566d8d709921afbaf27377f4687396059e43d94
Matiko-Codes <mmatiko243@gmail.com>
2023-02-01 18:20:30 +0300

    Task 0

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,40 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+  }
+  
+  const student1: Student = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25,
+    location: "New York"
+  };
+  
+  const student2: Student = {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 28,
+    location: "London"
+  };
+  
+  const studentsList: Student[] = [student1, student2];
+  
+  const table = document.createElement("table");
+  
+  studentsList.forEach(student => {
+    const row = document.createElement("tr");
+    const firstNameCell = document.createElement("td");
+    const locationCell = document.createElement("td");
+  
+    firstNameCell.innerHTML = student.firstName;
+    locationCell.innerHTML = student.location;
+  
+    row.appendChild(firstNameCell);
+    row.appendChild(locationCell);
+  
+    table.appendChild(row);
+  });
+  
+  document.body.appendChild(table);  
